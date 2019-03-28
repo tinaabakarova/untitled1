f48f530522b2a76cec15ff8cfe4a8d78d5ca36dd
tinaabakarova <tina.abakarova05@gmail.com>
2019-03-28 21:54:32 +0300

    рефакторинг

--- src/multithreading.java
+++ src/multithreading.java
@@ -23,17 +23,26 @@ public class multithreading {
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
 
-        ((Runnable) () -> {
+        Thread r = new Thread(((Runnable) () -> {
             for (int i = 0; i < h; i++) {
                 a1[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-        }).run();
-
-        ((Runnable) () -> {
+        }));
+        r.start();
+        Thread r2 = new Thread(((Runnable) () -> {
             for (int i = 0; i < h; i++) {
-                a2[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                a1[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-        }).run();
+        }));
+        r2.start();
+
+
+        try {
+            r.join();
+            r2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
