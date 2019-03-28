0dc8954b81be8530b3140c41b324f24d724f5987
FAbakarova <fatima.abakarova@aplana.com>
2019-03-28 15:55:24 +0300

    Initial commit

---
+++ src/qwert.java
@@ -0,0 +1,49 @@
+public class qwert {
+
+    static final int size = 10000000;
+    static final int h = size / 2;
+    float[] arr = new float[size];
+
+    public void method1() {
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public void method2() {
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        ((Runnable) () -> {
+            for (int i = 0; i < h; i++) {
+                a1[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        }).run();
+
+        ((Runnable) () -> {
+            for (int i = 0; i < h; i++) {
+                a2[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        }).run();
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void main(String[] args) {
+        new qwert().method1();
+        new qwert().method2();
+    }
+
+}
